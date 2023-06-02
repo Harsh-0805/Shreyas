7cec2dc8d9a6f60b074a8eddf4caa9724f2b7fe5
Harsh-0805 <harshbadala0805@gmail.com>
2023-06-02 19:44:01 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/DP2.png";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-import sample from '../assets/img/back3.mp4';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+// import sample from '../assets/img/back3.mp4';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["FrontEnd Web Developer", "Product Management", "UI/UX Designer", "Data Analytics"];
+  const toRotate = [
+    "FrontEnd Web Developer",
+    "Product Management",
+    "UI/UX Designer",
+    "Data Analytics",
+  ];
   const period = 2000;
 
   useEffect(() => {
@@ -20,33 +25,37 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -55,28 +64,51 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hello! I'm Shreyas`} <span className="txt-rotate" dataPeriod="100" data-rotate='["Front-End Web Developer","Product Manager","Data Analytics","UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>The Head of Growth and Strategy @E-Cell VNIT, Product Management Enthusiast,Buiness Analyst,Experienced React Devloper.
-                    Currently pursuing BTech in Civil Engineering from VNIT,Nagpur.Active member of gym club VNIT.Team player
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hello! I'm Shreyas`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="100"
+                      data-rotate='["Front-End Web Developer","Product Manager","Data Analytics","UI/UX Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    The Head of Growth and Strategy @E-Cell VNIT, Product
+                    Management Enthusiast,Buiness Analyst,Experienced React
+                    Devloper. Currently pursuing BTech in Civil Engineering from
+                    VNIT,Nagpur.Active member of gym club VNIT.Team player
                   </p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : "" }>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      
     </section>
-  )
-}
+  );
+};
